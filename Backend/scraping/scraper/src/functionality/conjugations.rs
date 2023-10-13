b69c308c55e4ffdb62a359590bba011714af6dae
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2023-10-14 00:32:01 +0100

    Update conjugations.rs

--- Backend/scraping/scraper/src/functionality/conjugations.rs
+++ Backend/scraping/scraper/src/functionality/conjugations.rs
@@ -110,6 +110,31 @@ pub async fn run_conjugations_modules() {
             let conjugation_data_vec: Vec<&str> = vec![rank, tense, subject, auxiliary, conjugate];
         }
     }
+
+
+
+    let bases_data: Vec<JsonData> = create_json_data_vec(bases_data_vec_vec, FieldOptions::BaseField);
+    let tenses_data: Vec<JsonData> = create_json_data_vec(tenses_data_vec_vec, FieldOptions::TenseField);
+    let subjects_data: Vec<JsonData> = create_json_data_vec(subjects_data_vec_vec, FieldOptions::SubjectField);
+    let auxiliaries_data: Vec<JsonData> = create_json_data_vec(auxiliaries_data_vec_vec, FieldOptions::AuxiliaryField);
+    let conjugate_data: Vec<JsonData> = create_json_data_vec(conjugates_data_vec_vec, FieldOptions:ConjugateField);
+    let conjugations_data: Vec<JsonData> = create_json_data_vec(conjugations_data_vec_vec, FieldOptions::ConjugationField);
+
+
+    let bases_file_path: &str = "temp/json/conjugations/bases.json";
+    save_data_to_json_file(&bases_data, bases_file_path);
+    let tenses_file_path: &str = "temp/json/conjugations/tenses.json";
+    save_data_to_json_file(&tenses_data, tenses_file_path);
+    let subjects_file_path: &str = "temp/json/conjugations/subjects.json";
+    save_data_to_json_file(&subjects_data, subjects_file_path);
+    let auxiliaries_file_path: &str = "temp/json/conjugations/auxiliaries.json";
+    save_data_to_json_file(&auxiliaries_data, auxiliaries_file_path);
+    let conjugates_file_path: &str = "temp/json/conjugations/conjugates.json";
+    save_data_to_json_file(&conjugates_data, conjugates_file_path);
+    let conjugations_file_path: &str = "temp/json/conjugations/conjugations.json";
+    save_data_to_json_file(&conjugations_data, conjugations_file_path);
+
+    save_data_to_postgres(&bases_data, &tenses_data, &subjects_data, &auxiliaries_data, &conjugates_data, &conjugations_data).await;
 }
 
 fn extract_languages(languages_data: Vec<JsonData>) -> Vec<&'static str> {
@@ -135,4 +160,210 @@ fn form_verb_url(languages) {
 
 fn form_conjugation_url(language: &str, verb: &str) -> String {
     return String::from("https::/conjugator.reverso.net/conjugation-") + language + "-verb-" + verb + ".html";
+}
+
+
+
+
+
+
+
+
+async fn save_data_to_postgres(bases_data: &Vec<JsonData>, tenses_data: &Vec<JsonData>, subjects_data: &Vec<JsonData>, auxiliaries_data: &Vec<JsonData>, conjugates_data: &Vec<JsonData>, conjugations_data: &Vec<JsonData>) {
+    let pool = create_pool_connection().await;
+
+    for bases_data in groups_data {
+        println!("{:?}, {:?}", group_data, group_data.pk);
+        if let Field::GroupField(GroupField{language, group}) = &group_data.fields {
+
+            //if unable to insert into table then update table else panic
+            let insert_query = sqlx::query("INSERT INTO verbs_group (id, language, group) VALUES ($1, $2, $3)")
+                .bind(group_data.pk)
+                .bind(language)
+                .bind(group)
+                .execute(&pool)
+                .await;
+
+            match insert_query {
+                Ok(res) => res,
+                Err(_) => {
+                    let rewrite_query = sqlx::query("UPDATE verbs_group SET language=($1), group=($2), WHERE id=($3)")
+                        .bind(language)
+                        .bind(group)
+                        .bind(group_data.pk)
+                        .execute(&pool).await;
+
+                    let rewrite_result = match rewrite_query {
+                        Ok(res) => res,
+                        Err(err) => panic!("Error: {:?}", err),
+                    };
+                    rewrite_result
+                },
+            };
+
+        } else {
+            panic!("non-group in group field");
+        };
+    }
+
+
+    for ending_data in endings_data {
+        if let Field::EndingField(EndingField { group, ending }) = &ending_data.fields {
+            // if unable to insert into table then update table else panic
+            let insert_query = sqlx::query("INSERT INTO verbs_ending (id, group, ending) VALUES ($1, $2, $3")
+                .bind(ending_data.pk)
+                .bind(group)
+                .bind(ending)
+                .execute(&pool).await;
+
+            match insert_query {
+                Ok(res) => res,
+                Err(_) => {
+                    let rewrite_query = sqlx::query("UPDATE verbs_ending SET group=($1), ending=($2), WHERE id=($3)")
+                        .bind(group)
+                        .bind(ending)
+                        .bind(ending_data.pk)
+                        .execute(&pool).await;
+
+                    let rewrite_result = match rewrite_query {
+                        Ok(res) => res,
+                        Err(err) => panic!("Error: {:?}", err),
+                    };
+                    rewrite_result
+                }
+            };
+        } else {
+            panic!("non-ending in ending field");
+        };
+    }
+    
+
+    for model_data in models_data {
+        println!("{:?} {:?}", model_data, model_data.pk);
+        if let Field::ModelField(ModelField { ending, model }) = &model_data.fields {
+            let insert_query = sqlx::query("INSERT INTO verbs_model (id, ending, model) VALUES ($1, $2, $3)")
+                .bind(model_data.pk)
+                .bind(ending)
+                .bind(model)
+                .execute(&pool).await;
+
+            match insert_query {
+                Ok(res) => res,
+                Err(_) => {
+                    let rewrite_query = sqlx::query("UPDATE verbs_model SET ending=($1), model=($2) WHERE id=($3)")
+                        .bind(ending)
+                        .bind(model)
+                        .bind(model_data.pk)
+                        .execute(&pool).await;
+
+                    let rewrite_result = match rewrite_query {
+                        Ok(res) => res,
+                        Err(err) => panic!("Error: {:?}", err),
+                    };
+                    rewrite_result
+                },
+            };
+        } else {
+            panic!("non-model in model field");
+        };
+    }
+
+
+
+
+    for group_data in groups_data {
+        println!("{:?}, {:?}", group_data, group_data.pk);
+        if let Field::GroupField(GroupField{language, group}) = &group_data.fields {
+
+            //if unable to insert into table then update table else panic
+            let insert_query = sqlx::query("INSERT INTO verbs_group (id, language, group) VALUES ($1, $2, $3)")
+                .bind(group_data.pk)
+                .bind(language)
+                .bind(group)
+                .execute(&pool)
+                .await;
+
+            match insert_query {
+                Ok(res) => res,
+                Err(_) => {
+                    let rewrite_query = sqlx::query("UPDATE verbs_group SET language=($1), group=($2), WHERE id=($3)")
+                        .bind(language)
+                        .bind(group)
+                        .bind(group_data.pk)
+                        .execute(&pool).await;
+
+                    let rewrite_result = match rewrite_query {
+                        Ok(res) => res,
+                        Err(err) => panic!("Error: {:?}", err),
+                    };
+                    rewrite_result
+                },
+            };
+
+        } else {
+            panic!("non-group in group field");
+        };
+    }
+
+
+    for conjugates_data in endings_data {
+        if let Field::EndingField(EndingField { group, ending }) = &ending_data.fields {
+            // if unable to insert into table then update table else panic
+            let insert_query = sqlx::query("INSERT INTO verbs_ending (id, group, ending) VALUES ($1, $2, $3")
+                .bind(ending_data.pk)
+                .bind(group)
+                .bind(ending)
+                .execute(&pool).await;
+
+            match insert_query {
+                Ok(res) => res,
+                Err(_) => {
+                    let rewrite_query = sqlx::query("UPDATE verbs_ending SET group=($1), ending=($2), WHERE id=($3)")
+                        .bind(group)
+                        .bind(ending)
+                        .bind(ending_data.pk)
+                        .execute(&pool).await;
+
+                    let rewrite_result = match rewrite_query {
+                        Ok(res) => res,
+                        Err(err) => panic!("Error: {:?}", err),
+                    };
+                    rewrite_result
+                }
+            };
+        } else {
+            panic!("non-ending in ending field");
+        };
+    }
+    
+
+    for conjugations_data in models_data {
+        println!("{:?} {:?}", model_data, model_data.pk);
+        if let Field::ModelField(ModelField { ending, model }) = &model_data.fields {
+            let insert_query = sqlx::query("INSERT INTO verbs_model (id, ending, model) VALUES ($1, $2, $3)")
+                .bind(model_data.pk)
+                .bind(ending)
+                .bind(model)
+                .execute(&pool).await;
+
+            match insert_query {
+                Ok(res) => res,
+                Err(_) => {
+                    let rewrite_query = sqlx::query("UPDATE verbs_model SET ending=($1), model=($2) WHERE id=($3)")
+                        .bind(ending)
+                        .bind(model)
+                        .bind(model_data.pk)
+                        .execute(&pool).await;
+
+                    let rewrite_result = match rewrite_query {
+                        Ok(res) => res,
+                        Err(err) => panic!("Error: {:?}", err),
+                    };
+                    rewrite_result
+                },
+            };
+        } else {
+            panic!("non-model in model field");
+        };
+    }
 }
