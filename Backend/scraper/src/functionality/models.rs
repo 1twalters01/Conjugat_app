b49dec45b11d8947445dbd603e58b829ef90d1ff
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2023-10-31 17:44:52 +0000

    Initial ending vec vec function

--- Backend/scraper/src/functionality/models.rs
+++ Backend/scraper/src/functionality/models.rs
@@ -123,16 +123,16 @@ fn get_model_html_vec(languages: Vec<String>) -> Vec<String> {
 
 
 fn get_groups_data_vec_vec(document_vec: &Vec<Html>) -> Vec<Vec<String>> {
-    let group_selector = scraper::Selector::parse("p[class=group]").unwrap();
     let all_selector = scraper::Selector::parse("div.model-row").unwrap();
+    let group_selector = scraper::Selector::parse("p[class=group]").unwrap();
     let mut groups_data_vec_vec: Vec<Vec<String>> = Vec::new();
    
     for (index, document) in document_vec.into_iter().enumerate() {
         let mut groups = document.select(&all_selector).into_iter()
-                .map(|all_scraped| all_scraped.select(&group_selector).next().unwrap().text()
-                    .collect::<Vec<_>>()).collect::<Vec<_>>()
-                .iter().filter(|testvec| testvec.len() > 0)
-                .map(|testvec| testvec[0]).collect::<Vec<_>>();
+            .map(|all_scraped| all_scraped.select(&group_selector).next().unwrap().text()
+                .collect::<Vec<_>>()).collect::<Vec<_>>()
+            .iter().filter(|testvec| testvec.len() > 0)
+            .map(|testvec| testvec[0]).collect::<Vec<_>>();
 
         groups.sort();
         groups.dedup();
@@ -147,6 +147,28 @@ fn get_groups_data_vec_vec(document_vec: &Vec<Html>) -> Vec<Vec<String>> {
 }
 
 
+fn get_endings_data_vec_vec(document_vec: &Vec<Html>) -> Vec<Vec<String>> {
+    let all_selector = scraper::Selector::parse("div.model-row").unwrap();
+    let ending_selector = scraper::Selector::parse("").unwrap();
+    let mut endings_data_vec_vec: Vec<Vec<String>> = Vec::new();
+    let mut ending_count: i64 = 0;
+
+    for (index, document) in document_vec.into_iter().enumerate() {
+        let mut endings_groups_vec = document.select(&all_selector).into_iter()
+            .map(|all_scraped| [all_scraped.select(&ending_selector).next.unwrap().text.collect::<Vec<_>>(),
+                                all_scraped.select(group_selector).next.unwrap().text.collect::<Vec<_>>()]) // use map to turn word into int
+            .collect::<Vec<_>>()
+            .iter().filter() // Make all empty groups be equal to "-"
+            .map(|testvecvec| testvecvec.map(|testvec| testvec[0]).collect::<Vec<_>>()).collect::<Vec<_>>();
+
+        endings_groups_vec.sort();
+        endings_groups_vec.dedup()
+    }
+
+    return ending_data_vec_vec;
+}
+
+    
 // need to fix let mut all on line 123
 // fn get_endings_data_vec_vec(document_vec: &Vec<Html>, groups_data: &Vec<JsonData>) -> Vec<Vec<String>> {
 //     let all_selector = scraper::Selector::parse("div.model-row").unwrap();
