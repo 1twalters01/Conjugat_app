af8a7223f963aa1467b732128d359e7ead4421e1
1twalters01 <1twalters01@gmail.com>
2023-11-16 01:01:52 +0000

    prototype group finished

--- Backend/scraper/src/functionality/models.rs
+++ Backend/scraper/src/functionality/models.rs
@@ -13,12 +13,15 @@ use crate::data_types::{
     }
 };
 
-use scraper::Html;
+use scraper::{
+    html::Select,
+    Html
+};
 
 use crate::helper_functions::{
     create_json_data_vec,
     save_data_to_json_file,
-    create_pool_connection,
+    // create_pool_connection,
     read_html_from_file,
     // async_scrape_html_from_url,
 };
@@ -34,45 +37,34 @@ use crate::helper_functions::{
 pub async fn run_model_module() {
     // get html vector for the models of each language saved
     let language_content: String = read_html_from_file("temp/json/languages/languages.json");
-    let (_languages_data, languages) = read_languages_from_file(language_content.as_str());
-    println!("{:?}", languages);
-    // let content_vec: Vec<String> = get_model_html_vec(languages).await;
+    let (_languages_data, languages) = read_language_data_from_json_data(language_content.as_str());
+
+    // get vector of all the content
     let content_vec: Vec<String> = get_model_html_vec(languages);
-
-
-    // Create the document vector
-    // let mut document_vec: Vec<Html> = Vec::new();
-    // for extract in &content_vec {
-    //     document_vec.push(scraper::Html::parse_document(&extract));
-    // }
-
     let document_vec: Vec<Html> = content_vec.into_iter()
         .map(|extract| scraper::Html::parse_document(&extract))
         .collect::<Vec<Html>>();
-    let all_selector = scraper::Selector::parse("div.model-row").unwrap();
-    let scraped_data = document_vec.map(|document|document.select(&all_selector));
+    let main_data: Vec<Select> = get_main_data(&document_vec);
 
-    // 0:language, 1: group
-    let groups_data_vec_vec: Vec<Vec<String>> = get_groups_data_vec_vec(&scraped_data);
-    println!("{:?}\n", groups_data_vec_vec);
+    // obtain the group data and group: language map, where 0: language, 1: group
+    let groups_data_vec_vec: Vec<Vec<String>> = get_groups_data_vec_vec(&main_data);
     let groups_data: Vec<JsonData> = create_json_data_vec(groups_data_vec_vec, FieldOptions::GroupField);
-    println!("{:?}\n", groups_data);
-    let groups_vec_vec_map: Vec<Vec<BTreeMap<String, String>>> = get_groups_vec_vec_map(groups_data);
-    println!("{:?}\n", groups_vec_map);
+    save_data_to_json_file(&groups_data, "temp/json/models/groups.json");
+    let groups_vec_vec_map: Vec<Vec<BTreeMap<String, i64>>> = get_groups_vec_vec_map(groups_data);
 
-    // // 0: group, 1: ending
-    // let endings_data_vec_vec: Vec<Vec<String>> = get_endings_data_vec_vec(&document_vec, &groups_data);
-    // let endings_data: Vec<JsonData> = create_json_data_vec(endings_data_vec_vec, FieldOptions::EndingField);
-    //
-    // // 0: ending, 1: model
+    // obtain the ending data and ending: group map, where 0: group, 1: ending
+    let endings_data_vec_vec: Vec<Vec<String>> = get_endings_data_vec_vec(&main_data, &groups_vec_vec_map);
+    let endings_data: Vec<JsonData> = create_json_data_vec(endings_data_vec_vec, FieldOptions::EndingField);
+    save_data_to_json_file(&endings_data, "temp/json/models/endings.json");
+    let endings_vec_vec_map: Vec<Vec<BTreeMap<String, i64>>> = get_endings_vec_vec_map(endings_data);
+
+    // obtain the model data and model: ending map, where 0: ending, 1: model
     // let models_data_vec_vec: Vec<Vec<String>> = get_models_data_vec_vec(&document_vec, &endings_data);
     // let models_data: Vec<JsonData> = create_json_data_vec(models_data_vec_vec, FieldOptions::ModelField);
+    // let models_vec_vec_map: Vec<Vec<BTreeMap<String, String>>> = get_models_vec_vec_map(models_data);
 
 
-    let groups_file_path: &str = "temp/json/models/groups.json";
-    save_data_to_json_file(&groups_data, groups_file_path);
     // let endings_file_path: &str = "temp/json/models/endings.json";
-    // save_data_to_json_file(&endings_data, endings_file_path);
     // let models_file_path: &str = "temp/json/models/models.json";
     // save_data_to_json_file(&models_data, models_file_path);
 
@@ -80,7 +72,7 @@ pub async fn run_model_module() {
 }
 
 
-fn read_languages_from_file(language_content: &str) -> (Vec<JsonData>, Vec<String>) {
+fn read_language_data_from_json_data(language_content: &str) -> (Vec<JsonData>, Vec<String>) {
     // let language_file_path: &str = "temp/json/models/groups.json";
     // language_content = read_html_from_file(language_file_path);
     let languages_data: Vec<JsonData> = serde_json::from_str(language_content).unwrap();
@@ -94,23 +86,6 @@ fn read_languages_from_file(language_content: &str) -> (Vec<JsonData>, Vec<Strin
     return (languages_data, languages);
 }
 
-// async fn get_model_html_vec(languages: Vec<String>) -> Vec<String> {    
-//     let mut urls: Vec<String> = Vec::new();
-//     for language in languages {
-//         urls.push(String::from("https://conjugator.reverso.net/conjugation-rules-model-") + language.as_str() + ".html");
-//     }
-//     println!("{:?}", urls);
-//
-//     // scrape the urls of their html
-//     let mut content_vec: Vec<String> = Vec::new();
-//     
-//     for url in urls {
-//         let content: String = async_scrape_html_from_url(url).await;
-//         content_vec.push(content);
-//     }
-//     
-//     return content_vec;
-// }
 
 fn get_model_html_vec(languages: Vec<String>) -> Vec<String> {
     let mut content_vec: Vec<String> = Vec::new();
@@ -118,7 +93,7 @@ fn get_model_html_vec(languages: Vec<String>) -> Vec<String> {
     for language in languages {
         let file_path: String = String::from("temp/html/models/") + language.to_lowercase().as_str() + ".txt";
         println!("{}", file_path);
-        let content = read_html_from_file(file_path.as_str());
+        let content: String = read_html_from_file(file_path.as_str());
         content_vec.push(content);
     }
 
@@ -126,26 +101,25 @@ fn get_model_html_vec(languages: Vec<String>) -> Vec<String> {
 }
 
 
-fn get_main_data(document_vec: &Vec<Html>) -> Vec<Vec<Strings>> {
-    let all_selector = scraper::Selector::parse("div.model-row").unwrap();
-    let main_data: Vec<Vec<String>> = Vec::new();
-    for document in document_vec.into_iter().enumerate() {
-        data = document.select(&all_selector).into_iter()
-            .map(|all_scraped| all_scraped).collect::<Vec<_>>();
-    }
+fn get_main_data(document_vec: &Vec<Html>) -> Vec<Select> {
+    let main_selector = scraper::Selector::parse("div.model-row").unwrap();
+    // let mut main_data: Vec<Html> = Vec::new();
 
+    let main_data = document_vec.iter().enumerate()
+    .map(|(index, document)| document.select(&main_selector)).collect::<Vec<_>>();
+    
     return main_data;
 }
 
-fn get_groups_data_vec_vec(document_vec: &Vec<Html>) -> Vec<Vec<String>> {
-    let all_selector = scraper::Selector::parse("div.model-row").unwrap();
+
+fn get_groups_data_vec_vec(main_data_vec: &Vec<Select>) -> Vec<Vec<String>> {
     let group_selector = scraper::Selector::parse("p[class=group]").unwrap();
     let mut groups_data_vec_vec: Vec<Vec<String>> = Vec::new();
    
-    for (index, document) in document_vec.into_iter().enumerate() {
-        let mut groups = document.select(&all_selector).into_iter()
-            .map(|all_scraped| all_scraped.select(&group_selector).next().unwrap().text()
-                .collect::<Vec<_>>()).collect::<Vec<_>>()
+    for (index, main_data) in main_data_vec.into_iter().enumerate() {
+        let mut groups = main_data.into_iter()
+            .map(|data| data.select(&group_selector).next().unwrap().text().collect::<Vec<_>>())
+            .collect::<Vec<_>>()
             .iter().filter(|testvec| testvec.len() > 0)
             .map(|testvec| testvec[0]).collect::<Vec<_>>();
 
@@ -162,15 +136,27 @@ fn get_groups_data_vec_vec(document_vec: &Vec<Html>) -> Vec<Vec<String>> {
 }
 
 
-fn get_groups_vec_vec_map(groups_data: Vec<JsonData>) -> Vec<Vec<BTreeMap<String, String>>> {
+use std::collections::BTreeMap;
+fn get_groups_vec_vec_map(groups_data: Vec<JsonData>) -> Vec<Vec<BTreeMap<String, i64>>> {
     // Outer vec by language
     // map os <group, pk>
-    let group_vec_vec_map: Vec<BTreeMap<String, String>> =
+    let mut group_vec_vec_map: Vec<Vec<BTreeMap<String, i64>>> = Vec::new(); 
     for group_data in groups_data {
-        
+        let mut group_map: BTreeMap<String, i64> = BTreeMap::new();
+        if let Field::GroupField(GroupField { group, language }) = &group_data.fields {
+            let language_id: i64 = language.parse::<i64>().unwrap();
+            group_map.insert(group.to_owned(), language_id);
+            if language_id > group_vec_vec_map.len().to_string().parse::<i64>().unwrap() {
+                group_vec_vec_map.push(Vec::from([group_map]));
+            } else {
+                group_vec_vec_map[language_id.to_string().parse::<usize>().unwrap()].push(group_map);
+            }
+        }
     }
+
     return group_vec_vec_map;
 }
+
 
 fn get_endings_data_vec_vec(document_vec: &Vec<Html>) -> Vec<Vec<String>> {
     let all_selector = scraper::Selector::parse("div.model-row").unwrap();
