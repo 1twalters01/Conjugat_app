1a5abedc1607bf45a8bfa06a0f52d738cbc31e59
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2024-01-13 17:08:13 +0000

    Update languages.rs

--- Backend/scraper/src/tests/languages.rs
+++ Backend/scraper/src/tests/languages.rs
@@ -1,1 +1,22 @@
 // test languages
+#[cfg(test)]
+use super::super::functionality::languages::*;
+//use crate::functionality::languages::*;
+
+#[test]
+fn languages_is_vector_valid {
+    let valid_vec_1: Vec<String> = Vec::from(["English", "French", "Italian", "Portuguese", "Spanish"]);
+    let valid_vec_2: Vec<String> = Vec::from(["Portuguese"]);
+  
+    let invalid_vec_1: Vec<String> = Vec::from([""]);
+    let invalid_vec_2: Vec<String> = Vec::new();
+    let invalid_vec_3: Vec<String> = Vec::from(["English", "Italian", "English"]);
+  
+    // Should check if it is a real language as well
+  
+    assert!(is_vector_valid(&valid_vec_1).ok());
+    assert!(is_vector_valid(&valid_vec_2).ok());
+    assert_eq(is_vector_valid(&invalid_vecor_1, "Vector has duplicated languages");
+    assert_eq(is_vector_valid(&invalid_vecor_2, "Vector has null element(s)");
+    assert_eq(is_vector_valid(&invalid_vecor_3, "Vector has null element(s)");
+}
