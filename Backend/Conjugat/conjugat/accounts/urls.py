5c4d3717a8ae307df59d008359ff1f2bdc86466d
1twalters01 <1twalters01>
2024-03-29 00:35:00 +0000

    get accounts endpoints api

--- Backend/Conjugat/conjugat/accounts/urls.py
+++ Backend/Conjugat/conjugat/accounts/urls.py
@@ -2,17 +2,17 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path("", views.GetRoutes.as_veiw(), name="get_routes"),
-    path("login/", views.Login.as_view(), name="login_username"),
-    path("login/totp/", views.LoginTotp.as_view(), name="login_password"),
-    path("login/oauth/", include("rest_social_auth.urls_knox")),
-    path("logout/", views.Logout.as_view(), name="logout"),
-    path("register/", views.Register.as_view(), name="register"),
-    path("activate/", views.Activate.as_view(), name="activate"),
-    path("password-reset/", views.PasswordReset.as_view(), name="pasword_reset"),
-    path(
-        "password-reset/confirm/",
-        views.PasswordResetConfirm.as_view(),
-        name="password_reset_confirm",
-    ),
+    path("", views.GetRoutes.as_view(), name="get_routes"),
+    # path("login/", views.Login.as_view(), name="login_username"),
+    # path("login/totp/", views.LoginTotp.as_view(), name="login_password"),
+    # path("login/oauth/", include("rest_social_auth.urls_knox")),
+    # path("logout/", views.Logout.as_view(), name="logout"),
+    # path("register/", views.Register.as_view(), name="register"),
+    # path("activate/", views.Activate.as_view(), name="activate"),
+    # path("password-reset/", views.PasswordReset.as_view(), name="pasword_reset"),
+    # path(
+    #     "password-reset/confirm/",
+    #     views.PasswordResetConfirm.as_view(),
+    #     name="password_reset_confirm",
+    # ),
 ]
--- Backend/Conjugat/conjugat/accounts/views.py
+++ Backend/Conjugat/conjugat/accounts/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.contrib.auth import login, logout
+from rest_framework import status, permissions
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class GetRoutes(APIView):
+    """
+    Routes
+    """
+
+    def get(self, request):
+        routes = [
+            {
+                "Endpoint": "/login/",
+                "Method": "POST",
+                "Description": "Login with username and password",
+            },
+            {"Endpoint": "/login/totp/", "Method": "POST", "Description": "totp login"},
+            {
+                "Endpoint": "/login/oauth/",
+                "Method": "POST",
+                "Description": "oauth login",
+            },
+            {"Endpoint": "/logout/", "Method": "POST", "Description": "logout"},
+            {
+                "Endpoint": "/register/",
+                "Method": "POST",
+                "Description": "register a new account",
+            },
+            {
+                "Endpoint": "/activate/",
+                "Method": "POST",
+                "Description": "activate an account",
+            },
+            {
+                "Endpoint": "/password-reset/",
+                "Method": "POST",
+                "Description": "reset password",
+            },
+            {
+                "Endpoint": "/password-reset/confirm/",
+                "Method": "POST",
+                "Description": "reset password change activation",
+            },
+        ]
+        return Response(routes, status=status.HTTP_200_OK)
